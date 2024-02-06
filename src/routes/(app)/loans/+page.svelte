4c103737590a49e42809978588821047f04f5bad
haykodarb <work@hayk.ar>
2024-02-05 21:04:05 -0300

    Finished Loans list page

--- src/routes/(app)/loans/+page.svelte
+++ src/routes/(app)/loans/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { getLoansWhere } from '$lib/tables/loans';
+	import { el } from 'date-fns/locale';
 	import Card from '../../../common/card.svelte';
+	import ThreeDotsVert from '../../../assets/icons/three_dots_vert.svelte';
 
 	export let data;
 
@@ -27,7 +29,7 @@
 	};
 </script>
 
-<div style="width: 100%;">
+<div class="main_container">
 	<div class="tab_bar">
 		<button on:click={selectOwned} class={`tab_button ${show_owned ? 'active' : ''}`}>Owned</button>
 		<button on:click={selectBorrowed} class={`tab_button ${show_borrowed ? 'active' : ''}`}
@@ -46,21 +48,38 @@
 					{#each loans.result as loan}
 						<div class="card_container">
 							<Card>
-								<div class="title">
-									{#if loan.books != null}
-										{loan.books.title}
-									{:else}
-										{loan.tools?.name}
-									{/if}
+								<div class="card_content">
+									<div class="card_top_row">
+										<div class="title">
+											{#if loan.books != null}
+												{loan.books.title}
+											{:else}
+												{loan.tools?.name}
+											{/if}
+										</div>
+										<button class="menu_button">
+											<ThreeDotsVert size="4vh" />
+										</button>
+									</div>
+
+									<div class="status">
+										Requested {#if data.user?.id == loan.loanee}
+											from {loan.owner_profile.username}
+										{:else}
+											by <button class="user">{loan.loanee_profile.username}</button>
+										{/if}
+									</div>
+									<div class="card_bottom_row">
+										<div class="status">
+											{#if loan.accepted}
+												Loan approved
+											{:else}
+												Pending Approval
+											{/if}
+										</div>
+										<div class="date">Sun, Jan 21</div>
+									</div>
 								</div>
-								<div class="user">
-									Requested {#if data.user?.id == loan.loanee}
-										from {loan.owner_profile.username}
-									{:else}
-										by {loan.loanee_profile.username}
-									{/if}
-								</div>
-								<div class="status"></div>
 							</Card>
 						</div>
 					{/each}
@@ -74,11 +93,38 @@
 			{:then loans}
 				{#if loans.result != null}
 					{#each loans.result as loan}
-						{#if loan.books != null}
-							{loan.books.title}
-						{:else}
-							{loan.tools?.name}
-						{/if}
+						<div class="card_container">
+							<Card>
+								<div class="card_content">
+									<div class="card_top_row">
+										<div class="title">
+											{#if loan.books != null}
+												{loan.books.title}
+											{:else}
+												{loan.tools?.name}
+											{/if}
+										</div>
+										<button class="menu_button">
+											<ThreeDotsVert size="4vh" />
+										</button>
+									</div>
+
+									<div class="status">
+										Requested from <button class="user">{loan.loanee_profile.username}</button>
+									</div>
+									<div class="card_bottom_row">
+										<div class="status">
+											{#if loan.accepted}
+												Loan approved
+											{:else}
+												Pending Approval
+											{/if}
+										</div>
+										<div class="date">Sun, Jan 21</div>
+									</div>
+								</div>
+							</Card>
+						</div>
 					{/each}
 				{/if}
 			{/await}
@@ -90,11 +136,32 @@
 			{:then loans}
 				{#if loans.result != null}
 					{#each loans.result as loan}
-						{#if loan.books != null}
-							{loan.books.title}
-						{:else}
-							{loan.tools?.name}
-						{/if}
+						<div class="card_container">
+							<Card>
+								<div class="card_content">
+									<div class="card_top_row">
+										<div class="title">
+											{#if loan.books != null}
+												{loan.books.title}
+											{:else}
+												{loan.tools?.name}
+											{/if}
+										</div>
+										<button class="menu_button">
+											<ThreeDotsVert size="4vh" />
+										</button>
+									</div>
+
+									<div class="status">
+										Requested {loan.loanee == data.user?.id ? 'from' : 'by'}
+										<button class="user">{loan.loanee_profile.username}</button>
+									</div>
+									<div class="card_bottom_row">
+										<div class="status">Returned Sun, Jan 21</div>
+									</div>
+								</div>
+							</Card>
+						</div>
 					{/each}
 				{/if}
 			{/await}
@@ -103,6 +170,13 @@
 </div>
 
 <style>
+	.main_container {
+		height: 100%;
+		width: 100%;
+		overflow: scroll;
+		box-sizing: border-box;
+	}
+
 	.tab_bar {
 		width: 50%;
 		height: 8vh;
@@ -121,28 +195,96 @@
 		cursor: pointer;
 		transition: 0.3s;
 	}
-    
-    .tab_button:hover {
+
+	.tab_button:hover {
 		background-color: var(--surface);
-        
-    }
+	}
 
 	.tab_button.active {
 		box-shadow: 0vh 0.3vh 0vh 0vh rgba(var(--primary-rgb), 1);
 	}
 
 	.body_content {
-		height: 100%;
 		width: 50%;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		box-sizing: border-box;
-		padding: 2%;
+		gap: 2vh;
+		padding-top: 4vh;
+		padding-bottom: 4vh;
 	}
 
 	.card_container {
-		height: 20vh;
 		width: 30vw;
+	}
+
+	.card_content {
+		padding: 3vh;
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		width: 100%;
+		box-sizing: border-box;
+	}
+
+	.card_top_row {
+		padding-bottom: 2vh;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	.card_bottom_row {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	.title {
+		font-size: 3vh;
+		font-weight: bold;
+	}
+
+	.user {
+		font-size: 2vh;
+		outline: none;
+		border: none;
+		background: none;
+		color: var(--primary);
+		cursor: pointer;
+		padding: 0px;
+		margin: 0px;
+		border-radius: 5%;
+	}
+
+	.user:hover {
+		background-color: var(--primary);
+		color: var(--onPrimary);
+	}
+
+	.status {
+		font-size: 2vh;
+	}
+
+	.date {
+		color: rgba(var(--onSurface-rgb), 0.5);
+	}
+
+	.menu_button {
+		background: none;
+		outline: none;
+		border: none;
+		cursor: pointer;
+		border-radius: 75%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		fill: var(--onSurface);
+	}
+
+	.menu_button:hover {
+		background-color: var(--surface);
+		fill: var(--primary);
 	}
 </style>
